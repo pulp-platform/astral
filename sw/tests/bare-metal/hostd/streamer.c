331c6bbfdec0a9baa9894f913c5ac38d4771346b
Yvan Tortorella <ytortorella@lagrev1.ee.ethz.ch>
2024-04-18 19:01:57 +0200

    Added proper streamer test. Wrong values read from register file.

--- sw/tests/bare-metal/hostd/streamer.c
+++ sw/tests/bare-metal/hostd/streamer.c
@@ -8,19 +8,67 @@
 
 #include "car_util.h"
 #include "printf.h"
-#include "hmr.h"
+
+#define PTME_ENABLE_ADDR (TCTM_STREAMER_CFG_PTME_BASE + 0x04)
+#define TM_FRAME_ADDR (TCTM_STREAMER_CFG_PTME_BASE + 0x48)
+#define PTME_CLK_PRESCALER_ADDR (TCTM_STREAMER_CFG_PTME_BASE + 0x68)
+#define BIT_CLK_DIVISOR_ADDR (TCTM_STREAMER_CFG_PTME_BASE + 0x6C)
+#define TME_INIT_ADDR (TCTM_STREAMER_CFG_PTME_BASE + 0x00)
+
+#define PTME_ENABLE_VALUE 0x00010000
+#define TM_FRAM_CFG_VALUE 0xFFFA0020
+#define PTME_CLK_PRESCALE_VALUE 0x00000000
+#define BIT_CLK_DIVISOR_VALUE 0x00000000
+#define TME_INIT_VALUE 0x0000AAAA
+#define PTME_ACTIVATE_VALUE 0x00000008
+#define PTME_DEACTIVATE_VALUE 0x00000000
 
 int main(void) {
 
-  // Hart 0 enters first
   if (hart_id() != 0) wfi();
 
-  printf("0x%x\n", TCTM_STREAMER_APB_TM_PACKET_BASE);
+  uint32_t error = 0;
 
-  writew(0x1, TCTM_STREAMER_APB_TM_PACKET_BASE);
-  writew(0x1, TCTM_STREAMER_CFG_MAP_ROUTER_BASE);
+  const char packet[] = { 0x09, 0x4C, 0xD2, 0x35,
+                          0x00, 0x1F, 0xAF, 0x01,
+                          0x00, 0x00, 0xAF, 0x02,
+                          0x00, 0x01, 0xAF, 0x03,
+                          0x00, 0x02, 0xAF, 0x04,
+                          0x00, 0x03, 0xAF, 0x05,
+                          0x00, 0x04, 0xAF, 0x06,
+                          0x00, 0x05, 0xAF, 0x07,
+                          0x00, 0x06, 0xAF, 0x08,
+                          0x00, 0x07 };
 
-  printf("Done\n");
+  // Enable PTME
+  writew(PTME_ENABLE_VALUE, PTME_ENABLE_ADDR);
+  error += (readw(PTME_ENABLE_ADDR) != PTME_ENABLE_VALUE);
+  // Configure telemetry frame
+  writew(TM_FRAM_CFG_VALUE, TM_FRAME_ADDR);
+  error += (readw(PTME_ENABLE_ADDR) != TM_FRAM_CFG_VALUE);
+  // Set PTME clock pre-scaler
+  writew(PTME_CLK_PRESCALE_VALUE, PTME_CLK_PRESCALER_ADDR);
+  error += (readw(PTME_CLK_PRESCALER_ADDR) != PTME_CLK_PRESCALE_VALUE);
+  // Set bit clock divisor
+  writew(BIT_CLK_DIVISOR_VALUE, BIT_CLK_DIVISOR_ADDR);
+  error += (readw(BIT_CLK_DIVISOR_ADDR) != BIT_CLK_DIVISOR_VALUE);
+  // TME_INIT (?)
+  writew(TME_INIT_VALUE, TME_INIT_ADDR);
+  error += (readw(TME_INIT_ADDR) != TME_INIT_VALUE);
 
-  return 0;
+  // Manifest interest in sending data (specifying only LSB is meaningful)
+  writew(PTME_ACTIVATE_VALUE, TCTM_STREAMER_APB_PTME_CFG + 0x00);
+
+  // Push data into the TM buffer
+  for (int i = 0; i < sizeof(packet); i++)
+    writew(packet[i], TCTM_STREAMER_APB_TM_PACKET_BASE + 0x04*i);
+
+  // Disable ACTIVE signal to start the transmission
+  writew(PTME_DEACTIVATE_VALUE, TCTM_STREAMER_APB_PTME_CFG);
+
+  // TODO: Should we wait for an event or something here?
+  if (error == 0) printf("Success!\n");
+  else printf("Failed!\n");
+
+  return error;
 }
