188d8986156addd497975a08f1cb2d9d544eaeaf
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-05-03 02:35:29 +0200

    Bump OpenTitan and align mbox test to chip config.

--- sw/tests/bare-metal/hostd/mbox_test.c
+++ sw/tests/bare-metal/hostd/mbox_test.c
@@ -39,18 +39,18 @@ int main(int argc, char const *argv[]) {
     asm volatile("csrw  mstatus, %0\n" : : "r"(global_irq_en  ));     // Set global interrupt enable in CVA6 csr
     asm volatile("csrw  mie, %0\n"     : : "r"(external_irq_en));     // Set external interrupt enable in CVA6 csr
     // PLIC setup
-    mmio_region_t plic_base_addr = mmio_region_from_addr(0x04000000);
+    mmio_region_t plic_base_addr = mmio_region_from_addr(&__base_plic);
     t = dif_rv_plic_init(plic_base_addr, &plic0);
     // Set two consecutive interrupts otherwise the SLINK breaks while loading the binary...
     for (int i = 0; i < 2; i++) {
-      t = dif_rv_plic_irq_set_priority(&plic0, IRQID+i*4, prio);
-      t = dif_rv_plic_irq_set_enabled(&plic0, IRQID+i*4, 0, kDifToggleEnabled);
+      t = dif_rv_plic_irq_set_priority(&plic0, IRQID+i*5, prio);
+      t = dif_rv_plic_irq_set_enabled(&plic0, IRQID+i*5, 0, kDifToggleEnabled);
     }
-    writew(0xBAADC0DE, 0x40000880);
-    a = readw(0x40000880);
+    writew(0xBAADC0DE, 0x40000280);
+    a = readw(0x40000280);
     if( a == 0xBAADC0DE )
-      writew(0x00000001, 0x40000804);    // ring doorbell if mailbox is accessible
-      writew(0x00000001, 0x4000080c);
+      writew(0x00000001, 0x40000204); // ring doorbell if mailbox is accessible
+      writew(0x00000001, 0x4000020C);
     wfi();
     return 0;
 }
@@ -59,8 +59,8 @@ void trap_vector (void){
    int * claim_irq;
    dif_rv_plic_irq_claim(&plic0, 0, &claim_irq);
    dif_rv_plic_irq_complete(&plic0, 0, &claim_irq);
-   writew(0x0, 0x40001204);
-   writew(0x0, 0x4000120C);
-   writew(0x1, 0x40001208);
+   writew(0x0, 0x40000D04);
+   writew(0x0, 0x40000D0C);
+   writew(0x1, 0x40000D08);
    return;
 }
