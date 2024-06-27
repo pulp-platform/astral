fefede098b30fefae14044f2a76348752257865c
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-06-27 16:56:52 +0200

    Bump non-free to add iommu and DMA est.

--- carfield.mk
+++ carfield.mk
@@ -47,7 +47,7 @@ include $(CAR_ROOT)/bender-safed.mk
 ######################
 
 CAR_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:astral/astral-nonfree.git
-CAR_NONFREE_COMMIT ?= 3ec9c5c184e656f312d08169d756333d3b30da44 # branch: master
+CAR_NONFREE_COMMIT ?= b1544c33a6fb078c70731102df3323c5fd891593 # branch: iommu
 
 ## @section Carfield platform nonfree components
 ## Clone the non-free verification IP for Carfield. Some components such as CI scripts and ASIC
