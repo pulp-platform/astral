2ff6420b2a12858730e60088ce727e112615978f
Yvan Tortorella <ytortorella@lagrev1.ee.ethz.ch>
2024-07-10 23:31:41 +0200

    Fix number of APB slaves.

--- hw/carfield_pkg.sv
+++ hw/carfield_pkg.sv
@@ -761,7 +761,7 @@ typedef logic [     AxiNarrowDataWidth-1:0] car_nar_dataw_t;
 typedef logic [        AxiNarrowStrobe-1:0] car_nar_strb_t;
 
 // APB Mapping
-localparam int unsigned NumApbMst = 9;
+localparam int unsigned NumApbMst = 7;
 
 typedef enum int {
   SystemTimerIdx   = 'd0,
