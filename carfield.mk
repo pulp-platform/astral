aa593708c277459ea978d59c4ec0e29eaf57390f
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-07-30 22:47:01 +0200

    Bump nonfree to run tech cells simulation in CI.

--- carfield.mk
+++ carfield.mk
@@ -48,7 +48,7 @@ include $(CAR_ROOT)/bender-safed.mk
 ######################
 
 CAR_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:astral/astral-nonfree.git
-CAR_NONFREE_COMMIT ?= fe9ab8b934c7aed99823ce965893ce9d49db54a2 # branch: master
+CAR_NONFREE_COMMIT ?= 132e33dbd4525ac491d13bb8f6898eaa9e8982c3
 
 ## @section Carfield platform nonfree components
 ## Clone the non-free verification IP for Carfield. Some components such as CI scripts and ASIC
