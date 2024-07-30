5afa17a8ca2a053284b820b5db117231fb544509
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-07-30 20:03:19 +0200

    Update tech commit.

--- carfield.mk
+++ carfield.mk
@@ -387,7 +387,7 @@ car-check-litmus-tests: $(LITMUS_WORK_DIR)/litmus.log
 ##############
 tech-repo := git@iis-git.ee.ethz.ch:Astral/gf12.git
 # no commit by default, change during development
-tech-commit := fdb28aed61aa113a897c6840674ec9033eb1f2ed # branch: zx/ecc_llc_256x39
+tech-commit := 26f9f03b6fcd9af8ace79bf5b4c6ea3a61c681a0 # branch: main
 
 tech-clone:
 	git clone $(tech-repo) tech
