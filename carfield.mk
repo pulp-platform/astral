e1df85d4cace3da8ad7c2a9474cb9cfa974f2e8b
Victor Isachi <victor.isachi@unibo.it>
2024-08-02 16:57:09 +0200

    Updated FLL branch pointer

--- carfield.mk
+++ carfield.mk
@@ -385,7 +385,7 @@ car-check-litmus-tests: $(LITMUS_WORK_DIR)/litmus.log
 # Technology #
 ##############
 tech-repo := git@iis-git.ee.ethz.ch:Astral/gf12.git
-tech-branch := vi/fll
+tech-branch := main
 tech-init:
 	git clone -b $(tech-branch) $(tech-repo) tech
 	$(MAKE) -C $(TECH_ROOT) init
