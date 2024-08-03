47500451b0076a91597d8cfff29fbe5e57bbe8cc
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-08-04 00:40:42 +0200

    Update tech commit.

--- carfield.mk
+++ carfield.mk
@@ -393,7 +393,7 @@ car-check-litmus-tests: $(LITMUS_WORK_DIR)/litmus.log
 ##############
 tech-repo := git@iis-git.ee.ethz.ch:Astral/gf12.git
 # no commit by default, change during development
-tech-commit := ec72d7736596842c34b5d76a45141e7a2fc6fe83 # branch: thales-rebase
+tech-commit := 1d9d97208abf6301208b24827bf57fe14ce4af91 # branch: thales-rebase
 
 tech-clone:
 	git clone $(tech-repo) tech
