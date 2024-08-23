57b8e36eee1cce88b429f0baa5a824f38b430a12
Maicol Ciani <maicol.ciani@unibo.it>
2024-08-23 16:12:22 +0200

    Update tech ref

--- carfield.mk
+++ carfield.mk
@@ -422,7 +422,7 @@ car-check-litmus-tests: $(LITMUS_WORK_DIR)/litmus.log
 ##############
 tech-repo := git@iis-git.ee.ethz.ch:Astral/gf12.git
 # no commit by default, change during development
-tech-commit := 4baeb28349422712f39a0887dbd2e06e098e499f # branch: yt/thales
+tech-commit := f2518b5d50ca03b5ca773839f0e4f48e88f31263 # branch: yt/thales
 
 tech-clone:
 	git clone $(tech-repo) tech
