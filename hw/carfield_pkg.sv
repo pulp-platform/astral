dd67cbcd54443e9008a13644ecfb7757c068ed6f
Luigi Ghionda <luigi.ghionda2@unibo.it>
2024-08-30 18:32:09 +0200

    Fix Clock Domains numbers

--- hw/carfield_pkg.sv
+++ hw/carfield_pkg.sv
@@ -319,7 +319,6 @@ function automatic int unsigned gen_carfield_domains(islands_cfg_t island_cfg);
   if (island_cfg.spatz.enable   ) begin ret++; end
   if (island_cfg.pulp.enable    ) begin ret++; end
   if (island_cfg.secured.enable ) begin ret++; end
-  if (island_cfg.ethernet.enable) begin ret++; end
   return ret;
 endfunction
 
@@ -399,7 +398,6 @@ function automatic carfield_domain_idx_t gen_domain_idx(islands_cfg_t island_cfg
   if (island_cfg.pulp.enable     ) begin ret.pulp    = i; i++; end
   if (island_cfg.spatz.enable    ) begin ret.spatz   = i; i++; end
   if (island_cfg.l2_port0.enable ) begin ret.l2      = i; i++; end
-  if (island_cfg.ethernet.enable ) begin ret.ethernet= i; i++; end
   return ret;
 endfunction
 
