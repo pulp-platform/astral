04b4243e7c488c3b449b2428661002435e848e3f
Luigi Ghionda <luigi.ghionda2@unibo.it>
2024-09-12 23:55:49 +0200

    Workaround: comment tb_MuSA_compact module to avoid top PS compilation issues

--- target/sim/src/vip_carfield_soc.sv
+++ target/sim/src/vip_carfield_soc.sv
@@ -104,7 +104,7 @@ module vip_carfield_soc
     .clk_o  ( clk   ),
     .rst_no ( rst_n )
   );
-  
+
 
   clk_rst_gen #(
     .ClkPeriod    ( ClkPeriodPeriph ),
@@ -395,20 +395,20 @@ module vip_carfield_soc
     .CADUOut ( ptme_enc_i )
   );
   */
-
+  /*
   tb_MuSA_compact i_tb_MuSA_compact (
       .CADUClk    ( ptme_clk_i),
       .CADUOut    ( ptme_enc_i),
       .HPC_ADDR   ( hpc_addr_i),
       .HPC_CMD_EN ( hpc_cmd_en_i),
-      .HPC_SMP    ( hpc_smp_i), 
+      .HPC_SMP    ( hpc_smp_i),
       .LLC_LINE   ( llc_line_i),
       .RST_BOARD  ( rst_n),
       .TCA        ( tc_active),
       .TCC        ( tc_clk),
       .TCS        ( tc_data)
    );
-
+  */
   spw_codec_tb i_spw_codec_tb (
       .DATA_IN   (spw_din),
       .STROBE_IN (spw_sin),
