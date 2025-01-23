e562fbc929de216e60605ad0515b1c07e22ba6de
Luigi Ghionda <luigi.ghionda2@unibo.it>
2025-01-23 10:57:33 +0100

    Add a simple non-iis env file

---
+++ env/env-non-iis.sh
@@ -0,0 +1,18 @@
+# Copyright 2022 ETH Zurich and University of Bologna.
+# Solderpad Hardware License, Version 0.51, see LICENSE for details.
+# SPDX-License-Identifier: SHL-0.51
+#
+
+# Set environment variables to choose of which island we have to compile the sw
+export PULPD_PRESENT=1
+export SAFED_PRESENT=0
+export SECURED_PRESENT=1
+export SPATZD_PRESENT=0
+
+if [ -z "${BENDER}" ]; then
+    echo "Environment variable 'BENDER' is not set."
+fi
+
+if ! which "riscv64-unknown-elf-gcc" >/dev/null 2>&1; then
+  echo "RISCV toolchain is NOT in the PATH."
+fi
