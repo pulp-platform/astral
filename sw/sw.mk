10133abaf49c678589bd2f023cce3dd4376e5af2
Luigi Ghionda <luigi.ghionda2@unibo.it>
2025-01-26 17:12:37 +0100

    sw: Enable offload tests compilation also on the spm

--- sw/sw.mk
+++ sw/sw.mk
@@ -99,6 +99,8 @@ define offload_tests_template
 		$(CHS_SW_OBJDUMP) -d -S $(4).$(basename $(notdir $(header))).car.l2.elf > $(4).$(basename $(notdir $(header))).car.l2.dump; \
 		$(CHS_SW_CC) $(CAR_SW_INCLUDES) -T$(CHS_LD_DIR)/dram.ld $(CAR_SW_LDFLAGS) -o $(4).$(basename $(notdir $(header))).car.dram.elf  $(4).$(basename $(notdir $(header))).car.o $(CHS_SW_LIBS) $(CAR_SW_LIBS); \
 		$(CHS_SW_OBJDUMP) -d -S $(4).$(basename $(notdir $(header))).car.dram.elf > $(4).$(basename $(notdir $(header))).car.dram.dump; \
+		$(CHS_SW_CC) $(CAR_SW_INCLUDES) -T$(CHS_LD_DIR)/spm.ld $(CAR_SW_LDFLAGS) -o $(4).$(basename $(notdir $(header))).car.spm.elf  $(4).$(basename $(notdir $(header))).car.o $(CHS_SW_LIBS) $(CAR_SW_LIBS); \
+		$(CHS_SW_OBJDUMP) -d -S $(4).$(basename $(notdir $(header))).car.spm.elf > $(4).$(basename $(notdir $(header))).car.spm.dump; \
 		$(RM) $(CAR_SW_DIR)/tests/bare-metal/$(2)/payload.h; \
 		$(RM) $(4).$(basename $(notdir $(header))).car.o; \
 	)
