81b2650f51dbc9b533e7cb4c06deacc6d4d0dd66
yvantor <yvan.tortorella@gmail.com>
2026-01-06 22:09:05 +0100

    Integrate TitanCFI

--- bender-common.mk
+++ bender-common.mk
@@ -10,11 +10,12 @@ CARFIELD_CONFIG ?= carfield_secure_periph
 
 # bender targets
 common_targs += -t cva6
-common_targs += -t mchan
 common_targs += -t cv32e40p_use_ff_regfile
 common_targs += -t scm_use_fpga_scm
 common_targs += -t cv64a6splus_imafdc_sv39_hpdcache_wb
 common_targs += -t rtl
+common_targs += -t idma
+common_targs += -t use_idma
 # The `snitch_cluster` target is needed for iDMA backend generation
 common_targs += -t snitch_cluster
 # The `deprecated` is needed for the redundancy cells, used as a
@@ -24,18 +25,18 @@ common_targs += -t deprecated
 common_targs += -t $(CARFIELD_CONFIG)
 
 # bender defines
-# common_defs += -D FEATURE_ICACHE_STAT
-# common_defs += -D PRIVATE_ICACHE
-# common_defs += -D HIERARCHY_ICACHE_32BIT
-# common_defs += -D ICAHE_USE_FF
-# common_defs += -D CLUSTER_ALIAS
-# common_defs += -D SNITCH_ICACHE
+common_defs += -D FEATURE_ICACHE_STAT
+common_defs += -D PRIVATE_ICACHE
+common_defs += -D HIERARCHY_ICACHE_32BIT
+common_defs += -D ICAHE_USE_FF
+common_defs += -D CLUSTER_ALIAS
+common_defs += -D SNITCH_ICACHE
 
 # Island exclusion
-ifeq ($(shell echo $(PULPD_PRESENT)), 0)
-common_targs += -e pulp_cluster
-else
+ifeq ($(shell echo $(PULPD_PRESENT)), 1)
 common_defs += -D PULPD_ENABLE
+else ifeq ($(shell echo $(SECURED_PRESENT)), 0)
+common_targs += -e pulp_cluster
 endif
 
 ifeq ($(shell echo $(SAFED_PRESENT)), 0)
