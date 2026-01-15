53e45d9e34a0e10bd1e14e584faab5daa42df7ab
yvantor <yvan.tortorella@gmail.com>
2026-01-15 17:57:20 +0100

    Enable GitLab CI

--- carfield.mk
+++ carfield.mk
@@ -48,8 +48,8 @@ include $(CAR_ROOT)/bender-safed.mk
 # Nonfree components #
 ######################
 
-CAR_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:astral/astral-nonfree.git
-CAR_NONFREE_COMMIT ?= 59aa6cfed0afe8a841c6fd478c07717a764fc4da
+CAR_NONFREE_REMOTE ?= git@gitlab.chips.it:digitalresearchline/scar-v/nonfree.git
+CAR_NONFREE_COMMIT ?= 00f4b0da3f4a096adfd5353184bd1db5a361a827 # main
 
 ## @section Carfield platform nonfree components
 ## Clone the non-free verification IP for Carfield. Some components such as CI scripts and ASIC
@@ -407,7 +407,7 @@ car-check-litmus-tests: $(LITMUS_WORK_DIR)/litmus.log
 ##############
 tech-repo := git@gitlab.chips.it:digitalresearchline/scar-v/gf22.git
 # no commit by default, change during development
-tech-commit := 90c3efd8d4d919a21ccc5fc019557b7f0508b5ac # branch: main
+tech-commit := 0b5d453704037a78f28ca14865e1e5c4644892e2 # branch: yt/gigi-rebased
 
 tech-clone:
 	git clone $(tech-repo) $(CAR_TECH_DIR)
